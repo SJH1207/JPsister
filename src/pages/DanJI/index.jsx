d90f2b066e6cfff1ed22c5f3bc10bdeba71c053b
SJH1207 <sjh576244995@gmail.com>
2023-10-26 20:30:08 +0800

    fix: 1

--- src/pages/DanJI/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SearchTable from '../components/searchTable';
-
-const DanJi = () => {
-  return <SearchTable teacher_name="单集" teacher_code="DanJi" collection="0" />;
-};
-
-export default DanJi;
